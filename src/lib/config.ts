78dfbb1c21d9bf7009d5aa82793b731339f8a37d
jjc4480 <jongchanjang90@gmail.com>
2026-04-10 10:29:55 +0900

    fix: rename blog title, remove bio from home, adjust bio tone

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
-	title: '개발 블로그',
+	title: 'jcjang의 블로그',
 	description: '개발하면서 겪은 고민들과 기록들을 남기는 공간.',
 	url: 'https://blog.jcjang.dev',
 	author: {
 		name: 'jcjang',
-		bio: '웹 기술 전반을 좋아하는 개발자. 요즘은 AI와 Agent 쪽도 파고 있습니다.',
+		bio: '웹 기술 전반을 좋아하는 개발자. 요즘은 AI와 Agent에도 관심을 가지고 있습니다.',
 		email: 'jongchangjang90@gmail.com'
 	},
 	defaultOgImage: '/og-default.png',
