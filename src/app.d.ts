0d2bc708bf0960c31d569d5244b3c1a9c8fca271
jjc4480 <jongchanjang90@gmail.com>
2026-04-13 11:47:06 +0900

    feat(draft): App.Locals/Platform 타입 정의 및 환경변수 주석
    - App.Locals: user (login, token) 추가
    - App.Platform: GitHub OAuth 및 세션 환경변수 타입 정의
    - wrangler.toml: Draft Editor 환경변수 주석 추가

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,17 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			user: { login: string; token: string } | null;
+		}
+		interface Platform {
+			env?: {
+				GITHUB_CLIENT_ID: string;
+				GITHUB_CLIENT_SECRET: string;
+				GITHUB_REPO: string;
+				ALLOWED_GITHUB_USER: string;
+				SESSION_SECRET: string;
+			};
+		}
 	}
 }
 
