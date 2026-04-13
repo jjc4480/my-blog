3ffabee44338bbec306fd956ce511768eb63cb14
jjc4480 <jongchanjang90@gmail.com>
2026-04-13 17:38:00 +0900

    feat: weave development philosophy (modularization, D&C, OOP) into all posts + fix SEO URL

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
 	title: 'jcjang의 블로그',
 	description: '개발하면서 겪은 고민들과 기록들을 남기는 공간.',
-	url: 'https://blog.jcjang.dev',
+	url: 'https://jcjang-blog.pages.dev',
 	author: {
 		name: 'jcjang',
 		bio: '웹 기술 전반을 좋아하는 개발자. 요즘은 AI와 Agent에도 관심을 가지고 있습니다.',
